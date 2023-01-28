35786830a1d7dfb5af31c0d43549f9038ec88d29
soubhagya2001 <122679633+soubhagya2001@users.noreply.github.com>
2023-01-29 00:34:55 +0530

    Update scriptcopy.js

--- scriptcopy.js
+++ scriptcopy.js
@@ -1,8 +1,8 @@
-var radius = 240; // how big of the radius 
+var radius = 340; // how big of the radius 
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
-var imgWidth = 120; // width of images (unit: px)
-var imgHeight = 170; // height of images (unit: px)
+var imgWidth = 220; // width of images (unit: px)
+var imgHeight = 270; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
 var bgMusicURL = 'https://od.lk/s/MTBfMjAzNTc0ODA5Xw/Atrangi%20Re%20Female%20Ver.mp3';
